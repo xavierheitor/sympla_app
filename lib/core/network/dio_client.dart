4f8c992ca24e2f6743f7c88960232191e055da01
Heitor Xavier <xavierheitor@hotmail.com>
2025-04-22 23:24:05 -0300

    erro get

--- lib/core/network/dio_client.dart
+++ lib/core/network/dio_client.dart
@@ -1,21 +1,27 @@
-import 'package:dio/dio.dart';
+import 'package:dio/dio.dart' as dio;
+import 'package:get/get.dart' as g;
 import 'package:sympla_app/core/constants/api_constants.dart';
 import 'package:sympla_app/core/logger/app_logger.dart';
+import 'package:sympla_app/core/session/session_manager.dart';
 
 class DioClient {
-  final Dio _dio;
+  final dio.Dio _dio;
 
   DioClient(String? Function() tokenProvider)
-      : _dio = Dio(BaseOptions(baseUrl: ApiConstants.baseUrl)) {
+      : _dio = dio.Dio(dio.BaseOptions(baseUrl: ApiConstants.baseUrl)) {
     _dio.interceptors.add(
-      InterceptorsWrapper(
+      dio.InterceptorsWrapper(
         onRequest: (options, handler) {
-          final token = tokenProvider();
+          final sessionManager = g.Get.find<SessionManager>();
+          final token = sessionManager.tokenSync;
           AppLogger.i('🔐 Token: $token');
+
           if (token != null && token.isNotEmpty) {
             options.headers['Authorization'] = 'Bearer $token';
+            options.headers['Content-Type'] = 'application/json';
             AppLogger.i('🔐 Token adicionado ao header: $token');
           }
+
           handler.next(options);
         },
       ),
@@ -23,22 +29,23 @@ class DioClient {
   }
 
   // Métodos simplificados
-  Future<Response> get(String path, {Map<String, dynamic>? queryParameters}) {
+  Future<dio.Response> get(String path,
+      {Map<String, dynamic>? queryParameters}) {
     return _dio.get(path, queryParameters: queryParameters);
   }
 
-  Future<Response> post(String path, {dynamic data}) {
+  Future<dio.Response> post(String path, {dynamic data}) {
     return _dio.post(path, data: data);
   }
 
-  Future<Response> put(String path, {dynamic data}) {
+  Future<dio.Response> put(String path, {dynamic data}) {
     return _dio.put(path, data: data);
   }
 
-  Future<Response> delete(String path) {
+  Future<dio.Response> delete(String path) {
     return _dio.delete(path);
   }
 
   // Expor Dio completo apenas se necessário
-  Dio get client => _dio;
+  dio.Dio get client => _dio;
 }
